b84259c623d0ef16ff3a51bafa290e895b4e043e
Steven Veshkini <stevenveshi@gmail.com>
2015-10-21 20:41:58 -0700

    modular

--- slack_cleaning_bot.py
+++ slack_cleaning_bot.py
@@ -11,8 +11,10 @@ def slack_cleaning_bot():
     slack = Slacker(os.environ['SLACK_KEY'])
     people = ['canzhi', 'stevensuckscock', 'neeloy', 'rohan', 'amillman', 'steven']
     day_of_year = datetime.now(tz=timezone('US/Pacific')).timetuple().tm_yday
-    slack.chat.post_message('#cleaning', "today is @" + str(people[day_of_year%6]) + "'s day to do the dishes. Tomorow is @" + str(people[(day_of_year+1)%6]))
-    david_message = sendgrid.Mail(to='david@dtbui.com', subject='👀 good shit go౦ԁ sHit👌 thats ✔ some good👌shit right👌there👌👌 rightthere ✔if i do ƽaү so my self', html='<strong> IT IS YOUR LUCKY DAY MOTHAFUCKA </strong>', text="It's" + str(people[day_of_year%6]) + "'s turn to clean the dishes!", from_email="shashank@thenothing.co")
+    message = "today is @" + str(people[day_of_year%6]) + "'s day to do the dishes. Tomorow is @" + str(people[(day_of_year+1)%6])
+    slack.chat.post_message('#cleaning', message)
+    im_david = '👀 good shit go౦ԁ sHit👌 thats ✔ some good👌shit right👌there👌👌 rightthere ✔if i do ƽaү so my self'
+    david_message = sendgrid.Mail(to='david@dtbui.com', subject=im_david, html=message, text=message, from_email="shashank@thenothing.co")
     if people[day_of_year%6] == 'rohan':
         message = sendgrid.Mail(to='rohanpai@berkeley.edu', subject='CLEAN THE DISHWASHER TODAY', html='<strong> IT IS YOUR LUCKY DAY MOTHAFUCKA</strong>', text='IT IS YOUR LUCKY DAY MOTHAFUCKA', from_email="shashank@thenothing.co")
         try:
